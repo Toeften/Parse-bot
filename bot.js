df12e0ab0406d4b85e2e51a86b6e782b33e99e6b
Toeften <78201060+Toeften@users.noreply.github.com>
2021-02-06 11:24:00 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
 const Discord = require('discord.js');
-const fetch = require("node-fetch")
+const request = require("request")
 const Tesseract = require('tesseract.js');
 const client = new Discord.Client();
 const prefix = "!";
 const whitelisted = [
-    'h',
-    'Lana Del Rey',
-    'The Senate',
-    'Dirkheads',
-    'Appellation',
-    'LittleHeroes',
-    'Nightingale',
-    'Garden',
-    'booling',
-    'Vovicum'
+	'h',
+	'Lana Del Rey',
+	'The Senate',
+	'Dirkheads',
+	'Appellation',
+	'LittleHeroes',
+	'Nightingale',
+	'Garden',
+	'booling',
+	'Vovicum'
 ]
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -24,57 +24,57 @@ client.on('message', async message => {
 		if (message.attachments.size > 0) {
 			if (message.attachments.every(attachIsImage)) {
 				message.attachments.forEach(async mstAttach => {
-                    let list = {
-                        notfound: [],
-                        crasher: [],
-                        whitelisted: []
-                    };
-                    let embed = await message.channel.send("Please wait...");
-                    let update = (list) => {
-                        embed.edit("Results!",{
-                            "embed": {
-                              "color": 16711680,
-                              "timestamp": new Date(),
-                              "fields": [
-                                {
-                                  "name": "Crashers",
-                                  "value": list.crasher.join(", ") || "Please wait..."
-                                },
-                                {
-                                  "name": "Whitelisted",
-                                  "value": list.whitelisted.join(', ') || "Please wait..."
-                                },
-                                {
-                                  "name": "Private Realmeye or Error",
-                                  "value": list.notfound.join(', ') || "Please wait..."
-                                }
-                              ],
-                              "footer": {
-                                text: 'this isnt 100% accurate but might help alot.',
-                              },
-                            }
-                        })
-                    }
-					Tesseract.recognize(mstAttach.attachment, 'eng',).then(({data: { text }}) => {
-                        let newtext = text.split("\n").join("")
-                        let players = newtext.slice(newtext.search(":") + 2).replace(/\s*/g,"").split(",");
-                        players.forEach(name => {
-                            fetch(`https://nightfirec.at/realmeye-api/?player=${name}`)
-                            .then(function(aaa) {
-                                return aaa.json();
-                              })
-                              .then(function(response) {
-                                if (response.error) {
-                                    console.log(name)
-                                    console.log(response.error)
-                                    list.notfound.push(name);
-                                    return;
-                                }
-                                if (whitelisted.includes(response.guild)) list.whitelisted.push(name);
-                                else list.crasher.push(name);
-                                update(list)
-                            });
-                        })
+					let list = {
+						notfound: [],
+						crasher: [],
+						whitelisted: []
+					};
+					let embed = await message.channel.send("Please wait...");
+					let update = (list) => {
+						embed.edit("Results!", {
+							"embed": {
+								"color": 16711680,
+								"timestamp": new Date(),
+								"fields": [{
+										"name": "Crashers",
+										"value": list.crasher.join(", ") || "Please wait..."
+									},
+									{
+										"name": "Whitelisted",
+										"value": list.whitelisted.join(', ') || "Please wait..."
+									},
+								  {
+										"name": "Private Realmeye or Error",
+										"value": list.notfound.join(', ') || "Please wait..."
+									}
+								],
+								"footer": {
+									text: 'this isnt 100% accurate but might help alot.',
+								},
+							}
+						})
+					}
+					Tesseract.recognize(mstAttach.attachment, 'eng', ).then(({
+						data: {
+							text
+						}
+					}) => {
+						let newtext = text.split("\n").join("")
+						let players = newtext.slice(newtext.search(":") + 2).replace(/\s*/g, "").split(",");
+						players.forEach(name => {
+							request(`https://nightfirec.at/realmeye-api/?player=${name}`, function(error, response, body) {
+								let playerData = JSON.parse(body)
+								if (playerData.error) {
+									console.log(name)
+									console.log(playerData.error)
+									list.notfound.push(name);
+									return;
+								}
+								if (whitelisted.includes(playerData.guild)) list.whitelisted.push(name);
+								else list.crasher.push(name);
+								update(list)
+							});
+						})
 					})
 				})
 			}
@@ -82,6 +82,9 @@ client.on('message', async message => {
 			message.reply("your image?????????")
 		}
 	}
+  if (message.content === prefix + "ping") {
+    message.channel.send("Ping: " + client.ws.ping);
+  }
 });
 
 function attachIsImage(msgAttach) {
